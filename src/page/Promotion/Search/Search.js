ea8f1f3f53c1deb21627b579bfdaf9de1f7bd38c
Diegooliveyra <diegooliveirabryto@gmail.com>
2021-03-27 21:05:36 -0300

    feito um useeffect para pegar os dados so db

--- src/page/Promotion/Search/Search.js
+++ src/page/Promotion/Search/Search.js
@@ -1,31 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PromotionCard from 'Components/Promotion/Card/Card';
 import axios from 'axios';
 import 'style/App.css';
 
-const promotion = {
-  id: 1,
-  title:
-    'Kit Notebook Acer Aspire 3 + Mochila Green, A315-41-R790, AMD Ryzen 3 2200U Dual Core',
-  price: 1799.0,
-  imageUrl:
-    'https://cdn.gatry.com/gatry-static/promocao/imagem_url/2631517face1861bc4f46ae154d387de.png',
-  url:
-    'https://www.amazon.com.br/Notebook-Acer-Mochila-A315-41-R790-Mem%C3%B3ria/dp/B07YDWLV7S/ref=as_li_ss_tl?ie=UTF8&linkCode=sl1&tag=gatry0b-20&linkId=e4a1146599e36741a720a6a952cbc328&language=pt_BR',
-  comments: [
-    {
-      id: 1,
-      comment: 'TELA HD',
-    },
-  ],
-};
-
 const PagePromotionSearch = () => {
-  axios.get('http://localhost:5000/promotions?_embed=comments ');
+  const [promotions, setPromotions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/promotions?_embed=comments')
+      .then((response) => setPromotions(response.data));
+  }, []);
 
   return (
     <div className="App">
-      <PromotionCard promotion={promotion} />
+      {promotions.map((promotion) => (
+        <PromotionCard key={promotion.id} promotion={promotion} />
+      ))}
     </div>
   );
 };
--- src/page/Root.js
+++ src/page/Root.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import PagePromotionForm from './Promotion/Form/Form';
 import PagePromotionSearch from './Promotion/Search/Search';
 
