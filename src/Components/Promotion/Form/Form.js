568fb7b87fcf8133341f7c9f4dbfaac4970d4683
diegooliveyra <diegooliveirabryto@gmail.com>
2021-04-05 21:41:48 -0300

    feito o formulario de edição

--- src/Components/Promotion/Form/Form.js
+++ src/Components/Promotion/Form/Form.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import axios from 'axios';
 import './Form.css';
 
-const price = {
+const InitialValue = {
   title: '',
   url: '',
   imageUrl: '',
   price: 0,
 };
 
-const PromotionForm = () => {
-  const [values, setValues] = useState(price);
+const PromotionForm = ({ id }) => {
+  const [values, setValues] = useState(id ? null : InitialValue);
   const history = useHistory();
+
+  useEffect(() => {
+    if (id) {
+      axios.get(`http://localhost:5000/promotions/${id}`).then((response) => {
+        setValues(response.data);
+      });
+    }
+  }, []);
 
   function onChange({ target }) {
     const { name, value } = target;
@@ -21,9 +29,12 @@ const PromotionForm = () => {
 
   function handleSubmit(e) {
     e.preventDefault();
-    axios
-      .post('http://localhost:5000/promotions', values)
-      .then((response) => history.push('/'));
+    const method = id ? 'put' : 'post';
+    const url = id
+      ? `http://localhost:5000/promotions/${id}`
+      : 'http://localhost:5000/promotions';
+
+    axios[method](url, values).then((response) => history.push('/'));
   }
 
   return (
@@ -32,32 +43,55 @@ const PromotionForm = () => {
         <h1>Nova Promoção</h1>
         <Link to="/">Voltar</Link>
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="promotion-form__group">
-          <label htmlFor="title">Titulo</label>
-          <input name="title" id="title" type="text" onChange={onChange} />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="url">Link</label>
-          <input name="url" id="url" type="text" onChange={onChange} />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="imageUrl">Imagem (URL)</label>
-          <input
-            name="imageUrl"
-            id="imageUrl"
-            type="text"
-            onChange={onChange}
-          />
-        </div>
-        <div className="promotion-form__group">
-          <label htmlFor="price">Preço</label>
-          <input name="price" id="price" type="number" onChange={onChange} />
-        </div>
-        <div className="promotion-form__button">
-          <button type="submit">Salvar</button>
-        </div>
-      </form>
+      {!values ? (
+        <div>Carregando...</div>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div className="promotion-form__group">
+            <label htmlFor="title">Titulo</label>
+            <input
+              name="title"
+              id="title"
+              type="text"
+              onChange={onChange}
+              value={values.title}
+            />
+          </div>
+          <div className="promotion-form__group">
+            <label htmlFor="url">Link</label>
+            <input
+              name="url"
+              id="url"
+              type="text"
+              onChange={onChange}
+              value={values.url}
+            />
+          </div>
+          <div className="promotion-form__group">
+            <label htmlFor="imageUrl">Imagem (URL)</label>
+            <input
+              name="imageUrl"
+              id="imageUrl"
+              type="text"
+              onChange={onChange}
+              value={values.imageUrl}
+            />
+          </div>
+          <div className="promotion-form__group">
+            <label htmlFor="price">Preço</label>
+            <input
+              name="price"
+              id="price"
+              type="number"
+              onChange={onChange}
+              value={values.price}
+            />
+          </div>
+          <div className="promotion-form__button">
+            <button type="submit">Salvar</button>
+          </div>
+        </form>
+      )}
     </section>
   );
 };
