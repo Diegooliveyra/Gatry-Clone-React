8c72f6e65f6fba5ebc8dc6b5db9477d9d96a1222
diegooliveyra <diegooliveirabryto@gmail.com>
2021-04-30 09:36:52 -0300

    corrigindo erro cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ noCors: true });
 
 const port = process.env.PORT || 5000;
 
